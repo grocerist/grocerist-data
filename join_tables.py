65d5c03c58f8d3cc38183d85395c68a041a8cf60
csae8092 <peter.andorfer@oeaw.ac.at>
2023-09-11 11:27:02 +0200

    join document with person table

--- join_tables.py
+++ join_tables.py
@@ -22,5 +22,19 @@ for key, value in source_data.items():
         x.pop("documents", "")
     value["goods"] = new_values
 
+
+seed_file = os.path.join(JSON_FOLDER, "persons.json")
+with open(seed_file, "r") as f:
+    seed_data = json.load(f)
+
+for key, value in source_data.items():
+    old_values = value["main_person"]
+    new_values = []
+    for x in old_values:
+        new_values.append(seed_data[f"{x['id']}"])
+    for x in new_values:
+        x.pop("documents", "")
+    value["main_person"] = new_values
+
 with open(source_file, "w") as f:
     json.dump(source_data, f, ensure_ascii=False, indent=4)
