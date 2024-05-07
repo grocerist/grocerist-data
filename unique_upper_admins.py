4afc04eff9d222a89b57ba9e8b590748fa0f945e
kisram <s2kisram@uni-trier.de>
2024-05-07 17:06:49 +0200

    fix logic for unique upper administration units

--- unique_upper_admins.py
+++ unique_upper_admins.py
@@ -10,18 +10,19 @@ for location in locations:
         os.path.join(JSON_FOLDER, location + ".json"), "r", encoding="utf-8"
     ) as f:
         data = json.load(f)
-        for key, value in data.items():
+        for key, entry in data.items():
             for i in range(1, 5):
-                if "upper_admin" + str(i) in value:
-                    # Use a set to store unique JSON objects
-                    unique_data = set(
-                        json.dumps(item, sort_keys=True)
-                        for item in value["upper_admin" + str(i)]
-                    )
-                    # Convert each JSON object back to a dictionary
-                    unique_data = [json.loads(item) for item in unique_data]
-                    # Replace the value with unique_data
-                    value["upper_admin" + str(i)] = unique_data
+                if "upper_admin" + str(i) in entry:
+                    unique_data = []
+                    seen_values = set()
+
+                    for item in entry["upper_admin" + str(i)]:
+                        if item["value"] not in seen_values:
+                            unique_data.append(item)
+                            seen_values.add(item["value"])
+
+                    entry["upper_admin" + str(i)] = unique_data
+                    print(entry["upper_admin" + str(i)])
 
     with open(
         os.path.join(JSON_FOLDER, location + ".json"), "w", encoding="utf-8"
