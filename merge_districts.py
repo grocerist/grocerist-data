ef576d302a2dcea57ae0fa67a17d6faeeb1e20b7
kisram <s2kisram@uni-trier.de>
2025-03-03 14:23:16 +0100

    merge Galatas in upper_admin1 as well

--- merge_districts.py
+++ merge_districts.py
@@ -36,8 +36,24 @@ def replace_entries(json_file, entries_to_replace):
         data = json.load(file)
 
     for value in data.values():
-        if (value["district"] and value["district"][0]["value"]) in entries_to_replace:
+
+        # case for documents.json (where there should only ever be one district entry)
+        if (
+            "district" in value
+            and value["district"]
+            and value["district"][0]["value"] in entries_to_replace
+        ):
             value["district"] = [{"id": 4, "value": "Galata"}]
+        # case for the location json files, where there can be multiple entries
+        elif "upper_admin1" in value and value["upper_admin1"]:
+            for district in value["upper_admin1"]:
+                if district["value"] in entries_to_replace:
+                    print("replacing", district["value"])
+                    district["ids"] = {
+                        "database_table_1488": 20,
+                        "database_table_1492": 4,
+                    }
+                    district["value"] = "Galata"
 
     with open(json_file, "w", encoding="utf-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
@@ -48,5 +64,14 @@ entries_to_merge = ["Galata Harici", "Galata Dahili"]
 districts_file = os.path.join("json_dumps", "districts.json")
 merge_entries(districts_file, merge_into, entries_to_merge)
 
-documents_file = os.path.join("json_dumps", "documents.json")
-replace_entries(documents_file, entries_to_merge)
+
+files_linked_to_districts = [
+    "documents.json",
+    "karye.json",
+    "nahiye.json",
+    "address.json",
+    "neighbourhoods.json",
+    "quarter.json",
+]
+for file in files_linked_to_districts:
+    replace_entries(os.path.join("json_dumps", file), entries_to_merge)
